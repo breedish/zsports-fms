29577f673439f7255997cd81ff58a5988f364c47
dzmitry.zenin <dzmitry.zenin@pegasus.ae>
2019-01-05 00:31:04 +0300

    init report

---
+++ src/main/kotlin/com/breedish/zsports/fms/FmsApplication.kt
@@ -0,0 +1,12 @@
+package com.breedish.zsports.fms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FmsApplication>(*args)
+}
+
