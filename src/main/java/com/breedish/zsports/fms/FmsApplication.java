1afcec55ff996ec3feba92582aa482b8ebdfaeb1
dzmitry.zenin <dzmitry.zenin@pegasus.ae>
2019-01-05 00:37:18 +0300

    init

---
+++ src/main/java/com/breedish/zsports/fms/FmsApplication.java
@@ -0,0 +1,14 @@
+package com.breedish.zsports.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsApplication.class, args);
+	}
+
+}
+
